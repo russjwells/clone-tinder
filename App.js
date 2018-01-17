590190615d949211dd35139163d546e357f0ed13
Russ Wells <russ@russdigital.com>
2018-01-16 21:56:54 -0800

    pulling profile pic from facebook graph

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
+
+const fbImage = 'https://graph.facebook.com/511275546/picture?height=500'
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.card}>
-        
+        <Image
+          style={{flex: 1}}
+          source={{uri: fbImage}}
+        />
       </View>
     )
   }
@@ -14,6 +19,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   card: {
     flex: 1,
+    overflow: 'hidden',
     backgroundColor: 'white',
     margin: 10,
     marginTop: 100,
