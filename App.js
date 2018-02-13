1b1202c4149093e71527351f3e4ef9c00171d2b8
Russ Wells <russ@russdigital.com>
2018-02-13 13:26:14 -0800

    pulling data from firebase

--- App.js
+++ App.js
@@ -14,11 +14,17 @@ firebase.initializeApp(firebaseConfig)
 export default class App extends Component {
   state = {
     profileIndex: 0,
+    profiles: [],
   }
 
   componentWillMount() {
     firebase.database().ref().child('users').once('value', (snap) => {
-      console.log('Data', snap.val())
+      let profiles = []
+      snap.forEach((profile)=>{
+        const {name, bio, birthday, id} = profile.val()
+        profiles.push({name, bio, birthday, id})
+      })
+      this.setState({profiles})
     })
   }
 
@@ -30,7 +36,7 @@ export default class App extends Component {
     const {profileIndex} = this.state
     return (
       <View style={{flex: 1}}>
-        {profiles.slice(profileIndex, profileIndex + 3).reverse().map((profile, i) => {
+        {this.state.profiles.slice(profileIndex, profileIndex + 3).reverse().map((profile, i) => {
           return (
             <Card 
               key = {profile.id}
@@ -43,48 +49,3 @@ export default class App extends Component {
     )
   }
 }
-
-const profiles = [
-  {
-    id: '259389830744794',
-    name: 'Candice',
-    birthday: '10/18/1986',
-    bio: 'Supermodel',
-  },
-  {
-    id: '720115413',
-    name: 'Alessandra',
-    birthday: '1/10/1989',
-    bio: 'Dancer',
-  },
-  {
-    id: '169571172540',
-    name: 'Miranda',
-    birthday: '12/12/1983',
-    bio: 'Doctor',
-  },
-  {
-    id: '1476279359358140',
-    name: 'Alissa',
-    birthday: '2/11/1990',
-    bio: 'Comedian',
-  },
-  {
-    id: '1140849052644433',
-    name: 'Behati',
-    birthday: '3/23/1991',
-    bio: 'Developer',
-  },
-  {
-    id: '912478262117011',
-    name: 'Rosie',
-    birthday: '9/4/1989',
-    bio: 'Artist',
-  },
-  {
-    id: '173567062703796',
-    name: 'Kendall',
-    birthday: '8/17/1992',
-    bio: 'Truck Driver',
-  },
-]
