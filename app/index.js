8b32de92d9b00c3e0d7b318ba6d60f4c0d569d8a
Russ Wells <russ@russdigital.com>
2018-02-14 12:46:04 -0800

    facebook login button

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import * as firebase from 'firebase'
 import Home from './screens/home'
+import Login from './screens/login'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDg4fI6UyL6hYvDHwELQ75pjwspQW8_kDA",
@@ -12,7 +13,7 @@ firebase.initializeApp(firebaseConfig)
 export default class App extends Component {
     render() {
         return (
-            <Home />
+            <Login />
         )
     }
 }
---
+++ app/screens/login.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import { View, StyleSheet } from 'react-native'
+import FacebookButton from '../components/facebookButton'
+
+export default class Login extends Component {
+    render(){
+        return(
+            <View
+                style={styles.container}
+            >
+                <FacebookButton 
+                    onPress={() => console.log('Pressed')}
+                />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
