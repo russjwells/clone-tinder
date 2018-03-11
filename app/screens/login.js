8e5d9a84dfbceadf56d642b8c8995e14219cd89f
Russ Wells <russ@russdigital.com>
2018-03-10 20:47:36 -0800

    authentication via firebase

--- app/screens/login.js
+++ app/screens/login.js
@@ -1,19 +1,27 @@
 import Expo from 'expo'
+import firebase from 'firebase'
 import React, {Component} from 'react'
 import { View, StyleSheet } from 'react-native'
 import FacebookButton from '../components/facebookButton'
 
 export default class Login extends Component {
 
+    authenticate = (token) => {
+        const provider = firebase.auth.FacebookAuthProvider
+        const credential = provider.credential(token)
+        return firebase.auth().signInWithCredential(credential)
+    }
+
     login = async () => {
         const ADD_ID = '1773849149576744'
         const options = {
-            permissions: ['public_profile']
+            permissions: ['public_profile', 'email']
         }
         const {type, token} = await Expo.Facebook.logInWithReadPermissionsAsync(ADD_ID, options)
         if (type === 'success') {
             const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`)
             console.log(await response.json())
+            this.authenticate(token)
         }
     }
 
