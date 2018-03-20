1173a5044ea9c0062fb002cf740353a61bd5c32b
Russ Wells <russ@russdigital.com>
2018-03-19 18:52:19 -0700

    adding a user in firebase

--- app/screens/login.js
+++ app/screens/login.js
@@ -12,6 +12,10 @@ export default class Login extends Component {
         return firebase.auth().signInWithCredential(credential)
     }
 
+    createUser = (uid, userData) => {
+        firebase.database().ref('users').child(uid).update(userData)
+    }
+
     login = async () => {
         const ADD_ID = '1773849149576744'
         const options = {
@@ -21,8 +25,9 @@ export default class Login extends Component {
         if (type === 'success') {
             const fields = ['id', 'first_name', 'last_name', 'gender', 'birthday', 'work']
             const response = await fetch(`https://graph.facebook.com/me?fields=${fields.toString()}&access_token=${token}`)
-            console.log(await response.json())
-            this.authenticate(token)
+            const userData = await response.json()
+            const {uid} = await this.authenticate(token)
+            this.createUser(uid, userData)
         }
     }
 
