40307fbc0492bac610ba1bb6ee09aa62c2cc8042
Russ Wells <russ@russdigital.com>
2018-04-22 15:18:02 -0700

    removed sexy awakening brand

--- app/screens/login.js
+++ app/screens/login.js
@@ -75,7 +75,7 @@ export default class Login extends Component {
         return(
             <View
                 style={styles.container}>
-                <Text style={styles.titleText}>Sexy Awakening</Text>
+                <Text style={styles.titleText}>clone-tinder</Text>
                 <Text style={styles.introText}>Your clothes conceal much of your beauty, yet they hide not the un-beautiful. And though you seek in garments the freedom of privacy you may find in them a harness and a chain. Would that you could meet the sun and the wind with more of your skin and less of your raiment, For the breath of life is in the sunlight and the hand of life is in the wind.</Text>
                 <View style={styles.loginArea}>
                 {this.state.showSpinner ? 
@@ -93,19 +93,19 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'space-around',
-        backgroundColor: '#e54560'
+        backgroundColor: '#ffffff'
         
     },
     titleText: {
         fontSize: 40,
         fontWeight: 'bold',
-        color: 'white',
+        color: 'black',
         marginTop: 60
     },
     introText: {
         fontSize: 18,
         margin: 50,
-        color: 'white',
+        color: 'black',
         lineHeight: 40,
         textAlign: 'center'
     },
